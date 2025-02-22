852a91931a0d39b3a5be2f62c47518dbfbd0a0e7
Dhannyphantom <youngskillzzz@gmail.com>
2025-02-22 13:17:30 +0100

    Basic Header nav component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState } from "react";
+
+import styles from "./Header.module.css";
+import Link from "next/link";
+import {
+  Calendar,
+  CalendarCheck,
+  CalendarCheck2,
+  CalendarClock,
+  MenuIcon,
+} from "lucide-react";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className={styles.container}>
+      <div className={styles.navContainer}>
+        <h1 className={styles.title}>
+          Jewelryhub<span className={styles.subtitle}>Gele</span>
+        </h1>
+
+        {/* <button className={styles.menuBtn} onClick={() => setIsOpen(!isOpen)}>
+          <MenuIcon size={24} />
+        </button> */}
+
+        <ul className={`${styles.navLinks} ${isOpen ? styles.show : ""}`}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Book</Link>
+          </li>
+          <li>
+            <Link href="/">Services</Link>
+          </li>
+          <li>
+            <Link href="/">About</Link>
+          </li>
+        </ul>
+        <div className={styles.bookIcon}>
+          <CalendarClock size={24} color="#aa60c8" />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
